93c3a21bc1dfa659aeec9199f6a9fb0fa26c36b5
michaelrealperson <65020305+michaelrealperson@users.noreply.github.com>
2023-03-07 14:33:38 +0300

    ProjectEuler
    ProjectEuler

---
+++ ProjectEuler/task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    internal class task1
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 1000;
+            int sum = SumOfNaturalMultiples(n);
+
+            Console.WriteLine($"The sum of the multiples of 3 and 5 to {n}  is {sum}");
+
+        }
+
+        static int SumOfNaturalMultiples (int n)
+        {
+
+            int sum = 0;
+
+            for (int i = 1; i < n; i++)
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            return sum;
+        }
+
+    }
+
+}
---
+++ ProjectEuler/task2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 4000000;
+            int sum = SumEvenFibonacciNumbers(n);
+
+            Console.WriteLine($"The sum of even Fibonacci numbers up to {n} is {sum}");
+
+        }
+
+        static int SumEvenFibonacciNumbers(int n)
+        {
+            int fibonacciLast = 2;
+            int fibonacciPrevious = 1;
+            int sum = 0;
+
+            while (fibonacciLast + fibonacciPrevious < n)
+            {
+                int reserve;
+
+                if ((fibonacciLast + fibonacciPrevious) % 2 == 0)
+                    sum += fibonacciLast + fibonacciPrevious;
+
+                // next numbers
+                reserve = fibonacciLast;
+                fibonacciLast += fibonacciPrevious;
+                fibonacciPrevious = reserve;
+            }
+
+            return sum;
+        }
+
+    }
+
+}
